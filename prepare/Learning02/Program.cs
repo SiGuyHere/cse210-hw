a4797ad1f69c321f017ac9ad4197c227048e5735
SiGuyHere <siiguyhere@gmail.com>
2025-09-30 14:23:15 -0500

    completed the learning assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job computerScientist = new Job();
+        computerScientist._jobTitle = "Computer Scientist";
+        computerScientist._company = "Computer Scientist";
+        computerScientist._startYear = "Computer Scientist";
+        computerScientist._endYear = "Computer Scientist";
+        Job biologist = new Job();
+        biologist._jobTitle = "Biologist";
+        biologist._company = "Biologist";
+        biologist._startYear = "Biologist";
+        biologist._endYear = "Biologist";
+        Resume silasResume = new Resume();
+        silasResume._name = "Silas";
+        silasResume._jobs.Add(biologist);
+        silasResume._jobs.Add(computerScientist);
+        silasResume.Display();
     }
 }
