d1159a24e5a658bf83176b3ca40a03ffc7740d11
SiGuyHere <siiguyhere@gmail.com>
2025-10-28 14:31:42 -0500

    prepare4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.ComponentModel.Design.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment thing = new MathAssignment("Silas Riday", "Fractions", "Chapter 4", "30-39");
+        Console.WriteLine(thing.GetSummary());
+        Console.WriteLine(thing.GetHomeworkList());
+        WritingAssignment thing2 = new WritingAssignment("Silas Riday", "History", "The super bloody war");
+        Console.WriteLine(thing2.GetSummary());
+        Console.WriteLine(thing2.GetWritingInformation());
     }
 }
